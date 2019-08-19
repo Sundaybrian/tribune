1af73c1325c6962759389d3921629afafc022ce7
Sunday Brian <brians931@gmail.com>
2019-08-19 12:26:07 +0300

    update the news_of_day funtion to use render and the newly created template

--- news/views.py
+++ news/views.py
@@ -8,16 +8,8 @@ def welcome(request):
 
 def news_of_day(request):
     date=dt.date.today()
-    day=convert_dates(date)
-
-    html=f'''
-    <html>
-        <body>
-            <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)    
+    
+    return render(request,'all-news/today-news.html',{'date':date})    
 
 def convert_dates(dates):
     '''
