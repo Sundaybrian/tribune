bdfe75b36b359621f45ac8a118ad6179235dbff0
Sunday Brian <brians931@gmail.com>
2019-08-19 11:05:04 +0300

    updated the new of the day to display also the day of the week from convert dates

--- news/views.py
+++ news/views.py
@@ -8,11 +8,12 @@ def welcome(request):
 
 def news_of_day(request):
     date=dt.date.today()
+    day=convert_dates(date)
 
     html=f'''
     <html>
         <body>
-            <h1>{date.day}-{date.month}-{date.year}</h1>
+            <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
         </body>
     </html>
     '''
