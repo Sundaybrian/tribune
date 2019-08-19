a31737cfe4f9e792ebb8cd6bdba2ef5052d89cb7
Sunday Brian <brians931@gmail.com>
2019-08-19 11:35:04 +0300

    add the past_days_news function that returns past news for a particular date

--- news/views.py
+++ news/views.py
@@ -32,3 +32,18 @@ def convert_dates(dates):
     day=days[day_number]
     return day  
 
+def past_days_news(request,past_date):
+    #converts data from the string url
+    date=dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day=convert_dates(date)
+    html=f'''
+    <html>
+        <body>
+            <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+
+
