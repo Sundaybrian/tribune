a2ff4cb9507893e365705b8bf689cdba6250cccf
Sunday Brian <brians931@gmail.com>
2019-08-19 10:35:55 +0300

    include the news url to the urls module

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sundaypriest/Desktop/MoPython/03_Django/The-Moringa-Tribune/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
