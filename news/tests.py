acb18cf95b40c2503b4c21fab4f744d7c4a8a294
Sunday Brian <brians931@gmail.com>
2019-08-21 17:07:14 +0300

    add the article tests

--- news/tests.py
+++ news/tests.py
@@ -19,5 +19,27 @@ class EditorTestClass(TestCase):
     def test_save_method(self):
         self.brian.save_editor()
         editors = Editor.objects.all()
-        self.assertTrue(len(editors) > 0)    
+        self.assertTrue(len(editors) > 0)  
+
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
 
