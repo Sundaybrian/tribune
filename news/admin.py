be11ad86172afc3138d3c907bf00f6c07cd92ac5
Sunday Brian <brians931@gmail.com>
2019-08-21 14:15:55 +0300

    registered the models in admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Editor,Article,Tags
 
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(Tags)
