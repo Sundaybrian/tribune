e87f2f108a44d06b3bfdf5c8d8a641eeff2cad07
Sunday Brian <brians931@gmail.com>
2019-08-22 10:25:51 +0300

    add a method to search articles by title

--- news/models.py
+++ news/models.py
@@ -44,8 +44,13 @@ class Article(models.Model):
     @classmethod
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
+        return news(
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        news=cls.objects.filter(title__icontains=search_term)
+
         return news
- 
 
         
 
