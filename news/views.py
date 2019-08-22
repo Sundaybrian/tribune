c553d896931cb39995afdb7e1c0d04a921146814
Sunday Brian <brians931@gmail.com>
2019-08-22 10:21:46 +0300

    add a search form to the navbar

--- news/views.py
+++ news/views.py
@@ -1,13 +1,15 @@
 import datetime as dt
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Article
 
 
 
 def news_of_day(request):
     date=dt.date.today()
+    news=Article.todays_news()
     
-    return render(request,'all-news/today-news.html',{'date':date})    
+    return render(request,'all-news/today-news.html',{'date':date,'news':news})    
 
 def convert_dates(dates):
     '''
@@ -35,6 +37,8 @@ def past_days_news(request,past_date):
     if date == dt.date.today():
         return redirect(news_of_day)    
 
-    return render(request,'all-news/past-news.html',{'date':date})
+    news=Article.days_news(date)    
+
+    return render(request,'all-news/past-news.html',{'date':date,'news':news})
 
 
