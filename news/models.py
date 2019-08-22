1b36e12a5f5521ed7d77aeda111c354905bbaf80
Sunday Brian <brians931@gmail.com>
2019-08-22 10:43:02 +0300

    update the navbar wit h bootstrap classes

--- news/models.py
+++ news/models.py
@@ -44,13 +44,14 @@ class Article(models.Model):
     @classmethod
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
-        return news(
+        return news
 
     @classmethod
     def search_by_title(cls,search_term):
         news=cls.objects.filter(title__icontains=search_term)
 
         return news
+       
 
         
 
