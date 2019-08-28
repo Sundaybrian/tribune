10b3f4d47c9413b62314db6ef09b621914eb476e
Sunday Brian <brians931@gmail.com>
2019-08-28 14:31:52 +0300

    add a view function to route to the new article creation page

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ import datetime as dt
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Article,NewsLetterRecipients
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm,NewsArticleForm
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
@@ -80,6 +80,24 @@ def article(request,article_id):
         article=Article.objects.get(id=article_id)
     except DoesNotExist:
         raise Http404()
-    return render (request,"all-news/article.html",{"article":article})          
+    return render (request,"all-news/article.html",{"article":article})   
+
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
+
+
 
 
