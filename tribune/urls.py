e0b53457c26a61948d2219bf9e9a696eb5c74f22
Sunday Brian <brians931@gmail.com>
2019-08-28 13:37:49 +0300

    add the base html to the templates directory

--- tribune/urls.py
+++ tribune/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url,include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('news.urls'))
+    url(r'',include('news.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
