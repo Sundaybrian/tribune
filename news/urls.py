e56831088fadf1c7d53765338fbe5f9eef9b6bb7
Sunday Brian <brians931@gmail.com>
2019-09-04 22:05:16 +0300

    add django rest framework

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,8 @@ urlpatterns=[
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name='pastNews'),
     url(r'^search/',views.search_results,name='search_results'),
     url(r'^article/(\d+)',views.article,name='article'),
-    url(r'^new/article$', views.new_article, name='new-article')
+    url(r'^new/article$', views.new_article, name='new-article'),
+    url(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
 
 ]
 
--- news/views.py
+++ news/views.py
@@ -5,27 +5,26 @@ from .models import Article,NewsLetterRecipients
 from .forms import NewsLetterForm,NewArticleForm
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 
 def news_of_day(request):
     date=dt.date.today()
     news=Article.todays_news()
-    
-    if request.method=='POST':
-        form=NewsLetterForm(request.POST)
-        if form.is_valid():
-            name=form.cleaned_data['your_name']
-            email=form.cleaned_data['email']
-            recipient=NewsLetterRecipients(name=name,email=email)
-            recipient.save()
+    form=NewsLetterForm() 
 
-            send_welcome_email(name,email)
+    return render(request,'all-news/today-news.html',{'date':date,'news':news,'letterForm':form}) 
 
-            HttpResponseRedirect('news_of_day')
+def newsletter(request):
+    name=request.POST.get('your_name')
+    email=request.POST.get('email')
 
-    else:
-        form=NewsLetterForm()    
-    return render(request,'all-news/today-news.html',{'date':date,'news':news,'letterForm':form})    
+    recipient=NewsLetterRecipients(name=name,email=email)
+    recipient.save()
+
+    send_welcome_email(name,email)
+    data={'success':'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
 def convert_dates(dates):
     '''
